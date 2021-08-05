d036a12f17aa4600393b5bf126e7bc38a790329e
Ofek Harel <ofekizh@gmail.com>
2021-08-05 15:34:24 +0300

    python netflix helper is ready needs to be implemented to app

--- app/src/main/python/netflix_helper/src/NetflixMovieExtract.py
+++ app/src/main/python/netflix_helper/src/NetflixMovieExtract.py
@@ -3,25 +3,34 @@ from xml.etree import ElementTree
 import xmltodict
 import os
 
-IN_OUT_DIT = r'A:\Software Dev\Projects\RandomPlay\netflix_helper\m_marvel.txt'
-DATABASE_DIR = r'A:\Software Dev\Projects\RandomPlay\netflix_helper\out\NetflixMovieData.txt'
+IN_OUT_DIR = r'A:\Software Dev\Projects\RandomPlay\Random-Play\app\src\main\res\raw\m_animated_disney.txt'
+DATABASE_DIR = r'A:\Software Dev\Projects\RandomPlay\Random-Play\app\src\main\python\netflix_helper\out\NetflixMovieData.txt'
 
-with open(IN_OUT_DIT, 'r+') as in_movies:
+with open(IN_OUT_DIR, 'r+') as in_movies:
     with open(DATABASE_DIR, 'r') as database:
         
         movies = in_movies.readlines()
         
         database_lines = database.readlines()
         database_movies = []
-        database_ids = []
+        database_ids = {}
         for line in database_lines:
-            database_movies.append(line[:line.find("|") - 1].lower().strip())
-            database_ids.append(line[:line.find("|")].replace("?", "|").lower().strip())
-            # print(line[:line.find("|") - 1].lower().strip())
+            name = line[:line.find("|") - 1].lower().strip()
+            movie_id = line[line.find("|") + 5:].replace("\n", "").strip()
+            database_movies.append(name)
+            database_ids.setdefault(name, movie_id)
+
 
         index = 0
         for movie in movies:
-            if movie.lower().strip().replace("\n", "") in database_movies:
-                movies[index] += "?"
-
+            name = movie.lower().strip().replace("\n", "")
+            if name in database_movies:
+                movies[index] = (name.title() + " ?" + database_ids[name] + '\n')
+                print(movies[index])
             index += 1
+        
+        in_movies.close()
+        os.remove(IN_OUT_DIR)
+        out = open(IN_OUT_DIR, "a")
+        out.writelines(movies)
+        out.flush
--- app/src/main/python/netflix_helper/src/NetflixSeriesExtract.py
+++ app/src/main/python/netflix_helper/src/NetflixSeriesExtract.py
@@ -3,30 +3,36 @@ from xml.etree import ElementTree
 import xmltodict
 import os
 
-series_name = input("Wanted Series: ")
-OUT_DIR = 's_{}.txt'.format(series_name.replace(" ", "_"))
-DATABASE_DIR = r'A:\Software Dev\Projects\RandomPlay\netflix_helper\out\NetflixSeriesData.txt'
+BASE_DIR = r'A:\Software Dev\Projects\RandomPlay\Random-Play\app\src\main\python\netflix_helper'
+DATABASE_DIR = os.path.join(BASE_DIR,  'out', 'NetflixSeriesData.txt')
 
 def get_series_id():
     with open(DATABASE_DIR, 'r') as database:
         for line in database.readlines():
             index = line.lower().find(series_name.lower())
             if index != -1:
-                return int(line[len(series_name) + 7:].replace("\n", ""))
+                return int(line[line.find("|") + 5:].replace("\n", ""))
         return -1
 
 
 def export_data_file(series_id):
-    with open(os.path.join(os.getcwd(), "netflix_helper", OUT_DIR), 'a') as out:
+    with open(OUT_DIR, 'x') as out:
 
         s_url = 'http://api.netflix.com/catalog/titles/series/{}/seasons'.format(series_id)
         response = requests.get(s_url)
         dict_data = xmltodict.parse(response.content)
         s_base_data = list(dict_data['catalog_titles'].values())[0]
         
+        is_first = True
         for s in s_base_data:
             s_index = s['title']['@regular']
-            out.write("\n" + str(s_index) + "\n")
+            season_str = ""
+            if is_first:
+                season_str = str(s_index) + "\n"
+                is_first = False
+            else:
+                season_str = "\n" + str(s_index) + "\n"
+            out.write(season_str)
 
             e_url = ""
             for url in s['link']:
@@ -49,10 +55,18 @@ def export_data_file(series_id):
 
 
 if __name__ == "__main__":
-    series_id = get_series_id()
-    if series_id == -1:
-        print("INVALID series -> Search for typos!")
-    else:
-        print("Exporting data...")
-        export_data_file(series_id)
-        print("\nDone!")
+    done = False
+    while not done:
+        series_name = input("Wanted Series: ")
+        
+        if series_name == 'q':
+            done = True
+            continue
+
+        OUT_DIR = os.path.join(BASE_DIR, 's_{}.txt'.format(series_name.replace(" ", "_").lower()))
+        series_id = get_series_id()
+        if series_id == -1:
+            print("INVALID series -> Search for typos!")
+        else:
+            export_data_file(series_id)
+            print("\nDone!\n")
